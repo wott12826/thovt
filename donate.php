fd156249bf9058937528c1c827d0d66687578f90
pusteugene <pustoviteugene@gmail.com>
2025-05-22 15:30:49 +0300

    first commit

---
+++ donate.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html><html lang="en" class="donation-banner-iframe"><head>
+<meta charset="utf-8"><title>Donate</title><style>body{margin:0}</style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script src="js/jquery-1.10.2.min.js"></script>
+      <script src="js/polyfill.min.js"></script>
+      <script src="js/athena.js"></script>
+          <script src="js/donation-banner.min.js" type="text/javascript"></script>
+    </head>
+    <body class="js-ia-iframe" style="margin:0">
+          <link href="css/banner.css" rel="stylesheet" type="text/css">
+    <link href="css/chevron.css" rel="stylesheet" type="text/css">
+
+    <header id="donate_banner" class="row js-donate-banner template-chevron background-default chevron-image-white  chevron-color-#F9BF3B formdesign-form" itemscope="" itemtype="http://schema.org/WPAdBlock" data-donationsourcedata="ctx=wb;uid=903e8312d520ef6cd35b032d366a4bc2;ref=https%3A%2F%2Fweb.archive.org%2Fweb%2F20240620164041%2Fhttps%3A%2F%2Fthovt.io%2F" data-exp="20250520BannerSizeTest" data-variant="WBTestAvgAmtText" data-platform="wb" data-baseurl="https://archive.org" data-bannertemplate="chevron" data-bannerbackground="default" data-snowfall="off" data-selectedtextcolor="#000000" data-selectedfillcolor="#F9BF3B" data-paymentoptionfillcolor="" data-paymentoptiontextcolor="" data-formbgcolor="#ffffff" data-formtextcolor="#000000" data-donatepageselectedtextcolor="#000000" data-donatepageselectedfillcolor="#F9BF3B" data-variantdollaramounts="[5, 15, 50]" data-variantamountlayout="multi-line" data-variantfrequencymode="checkbox" data-debugmode="false" style="--donationBannerFormTextColor: #000000;">
+      <button id="donate-close-button" class="donate-close" type="button" aria-label="Close banner">
+        <span class="sr-only">Close banner</span>
+        <img src="images/close-circle-dark.svg" id="banner-close-image-dark">
+        <img src="images/close-circle-white.svg" id="banner-close-image-white">
+      </button>
+      <div id="banner-body-container" style="background-color: #ffffff;">
+        <div id="banner-body-left-container" class="show-separator" style="--donationBannerChevronBG: #F9BF3B;">
+                    <div class="donate-body">
+                                      <div id="chevron-image-container" style="background-color: #ffffff;"></div>
+            
+            <div class="main-content">
+              <div class="appeal-text" style="--donationBannerAppealTextColor: #000000;">
+                <div class="desktop-body-content"><b>Can You Chip In?</b><br><br>Please don't scroll past this—the Wayback Machine is fighting for universal access to quality information. The Internet Archive, which runs this project, relies on online donations averaging about $15 to help us keep the record straight. We'd be deeply grateful if you'd join the one in a thousand users that support us financially.<br><br>We understand that not everyone can donate right now, but if you can afford to contribute this Wednesday, we promise it will be put to good use. Our resources are crucial for knowledge lovers everywhere—so if you find all these bits and bytes useful, <b>please pitch in.</b></div>
+                <div class="mobile-body-content"><b>Can You Chip In?</b> Please don't scroll past this. We keep the record straight by preserving government websites, news articles, historical data, and more. If you find our archive useful, <b>please pitch in.</b></div>
+              </div>
+
+                          </div>
+            <div id="donate-body-background-layer2" class="supplemental-background-layer"></div>
+            <div id="donate-body-background-layer1" class="supplemental-background-layer"></div>
+          </div>
+        </div>
+
+        <div class="donate-form js-donate-form" itemscope="" itemtype="http://schema.org/DonateAction">
+          <meta itemprop="recipient" content="Internet Archive">
+          <meta itemprop="actionStatus" content="PotentialActionStatus">
+          <div class="white-box">
+                                <div class="donation-form-container">
+        <donation-form-edit-donation amountoptions="[5, 15, 50]" defaultselectedamount="15" amountselectionlayout="multi-line" frequencyselectionmode="checkbox" stepnumbermode="hidenumbers" style="--paymentButtonSelectedFontColor: #000000;--paymentButtonSelectedColor: #F9BF3B;--formSectionTextColor: #000000;">
+        </donation-form-edit-donation>
+
+        <div class="cta">
+          <button class="cta-button" id="continue-button" style="color: #ffffff;background-color: #24765E;">
+            Continue
+          </button>
+                <div class="donate-later-container">
+        <a id="donate_later" href="#donate_dropdown" data-source-context="WB" data-event-click-tracking="DonateBannerWB|MaybeLaterLink" data-event-tracking-options="{ " service":="" "ao_no_sampling"="" }"="">Remind Me</a>
+        <div class="donate-later-wrap">
+          <div class="donate-later-dropdown js-donate-later-dropdown" id="donate_dropdown">
+            <button id="close_donate_dropdown" class="donate-close" type="button" aria-label="Close banner"><span class="iconochive-remove-circle"></span>
+            </button>
+            <form id="donate_reminder_form" data-base-url="https://archive.org">
+              <h5>Send me an email reminder</h5>
+              <label for="email">Email address (required)</label>
+              <input type="email" id="email" name="email" value="">
+              <div class="error-email">Please enter a valid email address.</div>
+              <label for="first-name">First name</label>
+              <input type="text" id="first-name" name="first_name" value="">
+              <label for="last-name">Last name</label>
+              <input type="text" id="last-name" name="last_name" value="">
+              <button type="submit">Submit</button>
+              <p>
+                By submitting, you agree to receive donor-related emails from the Internet Archive.
+                Your privacy is important to us. We do not sell or trade your information with anyone.
+              </p>
+            </form>
+            <div class="reminder-success hidden" id="donate_reminder_success" aria-live="polite">
+            </div>
+          </div>
+        </div>
+      </div>
+            </div>
+      </div>
+                          </div> <!--/.white-box -->
+          <div class="donate-form-chevrons" style="--donationBannerChevronBG: #F9BF3B;"></div>
+        </div> <!-- .donate-form -->
+      </div>
+    </header>
+          
+  
+</body></html>
